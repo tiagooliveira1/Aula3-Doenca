06ee25aab5d9876c760714dba6e66867bd00446e
Tiago Oliveira <tiago@econtabil.com>
2017-03-09 22:36:01 -0300

    Commit inicial

---
+++ src/com/teste/Aula3Doenca.java
@@ -0,0 +1,13 @@
+package com.teste;
+
+public class Aula3Doenca
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+        /*  Implementar. Professor desligou o computador kkkk*/
+
+    }
+
+}
